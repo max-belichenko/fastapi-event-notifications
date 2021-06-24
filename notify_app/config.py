c78ab0c9dea38a8eb54748e0fe5080fe7ca3643f
Max Belichenko <max.belichenko@mail.ru>
2021-06-24 12:06:49 +0300

    Refactor code

--- notify_app/config.py
+++ notify_app/config.py
@@ -1,13 +1,13 @@
 # Redis server configuration
 
-REDIS_HOST = "127.0.0.1"
+REDIS_HOST = "*"
 REDIS_PORT = 6379
 
 
 # SMTP server configuration
 
-SMTP_SERVER_HOSTNAME = 'smtp.mail.ru'
+SMTP_SERVER_HOSTNAME = '*'
 SMTP_SERVER_PORT = 465
-SMTP_USER = 'panther_lxi@mail.ru'
-SMTP_PASSWORD = 'TCJTcg8bvFC4WFVkT6oE'
-SMTP_FROM_ADDRESS = 'panther_lxi@mail.ru'
+SMTP_USER = '*'
+SMTP_PASSWORD = '*'
+SMTP_FROM_ADDRESS = '*'
